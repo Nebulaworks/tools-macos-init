ccb87cf3f871674fef120a9a33cc50dff646b5b9
joshua-bradley <jb@joshuabradley.net>
2017-11-26 14:10:03 -0800

    repo seed

---
+++ osx-init-upgrayedd.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+
+##
+# title             : NWI MacOS bootstrapper
+# file              : shell script
+# created           : 2017-11
+# modified          : ----------
+# www-site          : http://www.nebulaworks.com
+# description       : Nebulaworks Mac OS configuration script
+# 										run script in terminal as standard user
+# executor-version  : bash
+##
+
+###
+# Nebulaworks Mac OS configuration script
+# run script in terminal as a priviliged user
+###
+
+set -e
+
+###
+# Common tool source list for base systems
+###
+
+# base conmmon cli tools
+base_cli=(
+	'coreutils' 'diffutils' 'gnu-indent --with-default-names' 'gnu-sed --with-default-names'
+	'gnu-tar --with-default-names' 'gnu-which --with-default-names' 'ed --with-default-names'
+	'findutils --with-default-names' 'grep --with-default-names' 'wdiff --with-gettext'
+	'ccat' 'gawk' 'gnutls' 'gzip' 'unzip' 'watch' 'wget' 'bash' 'less' 'most' 'make'
+	'git'
+);
+
+# base common cask tools
+base_cask=(
+	'slack' 'google-drive-file-stream' 'google-chrome' '1password' 'zoom'
+);
+
+###
+# Engineering tool source list for base systems
+###
+
+# engineering cli tools
+eng_cli=(
+	'shellcheck' 'file-formula' 'rsync' 'jq' 'httpie' 'htop-osx' 'nmap' 'tmux' 'openssh'
+	'vim --with-override-system-vi --with-lua --without-perl' 'zsh' 'kubectl' 'awscli'
+);
+
+# engineering cask tools
+eng_cask=(
+	'docker' 'iterm2' 'tunnelblick' 'atom' 'firefox'
+);
+
+command_exists() {
+	command -v "$@" > /dev/null 2>&1
+}
+
+if [ -z "$1" ]
+then
+  echo "Usage:  $0 COMMAND"
+  echo ""
+  echo "minikube installer"
+  echo ""
+  echo "Commands:"
+  echo "  install     installs the minikube software stack"
+  echo "  cleanup     uninstalls the minikuve software stack"
+  exit $E_ARG_ERR
+fi
+
+brew_install() {
+	brew install "$@"
+}
+
+brew_cask_install() {
+	brew cask install "$@"
+}
+
+if ! command_exists 'brew'; then
+  echo 'Info: brew is not installed.' >&2
+  echo 'Installing HomeBrew package manager.... '
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# install oh-my-zsh framework
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
---
+++ test.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+#if (( EUID != 0 )); then
+#   echo "You must be root to properly run this script." 1>&2
+#   exit 100
+#fi
+
+list="cat dog fish"
+
+if [ -z "$1" ]
+then
+  echo "Usage:  $0 COMMAND"
+  echo ""
+  echo "minikube installer"
+  echo ""
+  echo "Commands:"
+  echo "  install     installs the minikube software stack"
+  echo "  cleanup     uninstalls the minikuve software stack"
+  exit $E_ARG_ERR
+fi
+
+for l in $list; do
+	echo $l
+done
+
+
+fancy_echo() {
+  local fmt="$1"; shift
+
+  # shellcheck disable=SC2059
+  printf "\n$fmt\n" "$@"
+}
+
+fancy_echo "this is my message"
+
+app=('coreutils'
+		'diffutils'
+		'gnu-indent --with-default-names'
+		'gnu-sed --with-default-names'
+		'gnu-tar --with-default-names'
+		'gnu-which --with-default-names'
+		'ed --with-default-names'
+		'findutils --with-default-names'
+		'grep --with-default-names'
+		'wdiff --with-gettext'
+		'ccat'
+		'gawk');
+
+for a in "${app[@]}"; do
+	echo $a
+done
+
+command_exists() {
+	command -v "$@" > /dev/null 2>&1
+}
+
+brew_install() {
+	brew info "$@"
+}
+
+if ! command_exists "it"; then
+	echo 'no brew here'
+else
+	echo 'Yay! you can brew'
+fi
+
+for a in "${app[@]}"; do
+	brew_install "$a"
+done
